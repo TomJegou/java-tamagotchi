604057ba11cefac3f2f7606343e844380fcc070b
L0uxe <loulourxl@hotmail.com>
2023-05-22 11:55:55 +0200

    testing unit time

--- src/main/java/com/ynov/tamagotchi/gameEngine/gameEngine.java
+++ src/main/java/com/ynov/tamagotchi/gameEngine/gameEngine.java
@@ -1,5 +1,23 @@
 package com.ynov.tamagotchi.gameEngine;
 
+import java.util.concurrent.TimeUnit;
+
 public class gameEngine {
+    final static int oneUnitTime = 1 ;
     
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+        try {
+            TimeUnit.SECONDS.sleep(oneUnitTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+ 
+        System.out.println("Hello World");
+    }
+
+
 }
