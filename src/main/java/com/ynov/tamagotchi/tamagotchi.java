02b5370f1fa7a06759194f0173bcb9a133ce1d9b
fpierrea <pierrealexis.favril@ynov.com>
2023-05-22 11:44:53 +0200

    class tamagotchi create

---
+++ src/main/java/com/ynov/tamagotchi/tamagotchi.java
@@ -0,0 +1,15 @@
+package com.ynov.tamagotchi;
+
+public class tamagotchi {
+    String name;
+    int happiness;
+    boolean hunger;
+    int cleanness;
+
+    public void Eat(){
+        this.hunger = false;
+    }
+    
+}
+
+
