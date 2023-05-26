942d9f39c1911752f3e9283e52f96c4ad4b6a661
L0uxe <loulourxl@hotmail.com>
2023-05-26 11:19:45 +0200

    wip

--- src/main/java/com/ynov/tamagotchi/gameEngine/gameEngine.java
+++ src/main/java/com/ynov/tamagotchi/gameEngine/gameEngine.java
@@ -2,8 +2,22 @@ package com.ynov.tamagotchi.gameEngine;
 
 import java.util.concurrent.TimeUnit;
 
+
 public class gameEngine {
-    final static int oneUnitTimeInSecond = 1 ;
+    private enum etapedevie {
+        EOUF,
+        BEVE,
+        ADULT,
+        Veillard,
+        MORT
+    }
+
+    private etapedevie currentStage;
+    private int happiness;
+    private int hunger;
+    private int dirtyLevel;
+    private boolean isSick;
+  
     
     public static void wait(int time,int unitTime)
     {
@@ -17,9 +31,14 @@ public class gameEngine {
 
     public static void main(String args[])
     {
-        System.out.println("Hello World");
-        wait(oneUnitTimeInSecond,1);
-        System.out.println("Hello World");
+        // Oeuf
+        System.out.println("Bienvenue sur Tamagotchi attendez que votre oeuf soit éclo");
+        wait(second,1);
+        System.out.println("L'oeuf à éclo");
+        // bebe
+        // Adult
+        // Veillard
+        // Mort
     }
 
 }
